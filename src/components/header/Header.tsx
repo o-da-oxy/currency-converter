1cc3093a73cc01c5e693868f2456d91d6ff5d27f
Oksana <demskaya01@gmail.com>
2023-04-03 21:34:47 +0300

    app name fix

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,7 +4,7 @@ import s from './Header.module.css';
 function Header() {
   return (
     <div className={s.header}>
-      <h1 className={s.name}>Currency Convertor</h1>
+      <h1 className={s.name}>Currency Converter</h1>
     </div>
   );
 }
